af4ba194340962eeb9cac6d9cb1543a4d7e8ae82
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-02-27 10:06:00 -0500

    Added code for deleting, filtering, and getting nested resources with axios.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  // GET a resource
+  // Get a resource
   axios
     .get("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error))
+    .then((response) => console.log("A Resource:", response))
+    .catch((error) => console.log("A Resource:", error))
     .finally(/* always executed */);
 
   // Get all resources
   axios
     .get("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error))
+    .then((response) => console.log("All Resources:", response))
+    .catch((error) => console.log("All Resources:", error))
     .finally(/* always executed */);
 
   // Create a resource
@@ -22,8 +22,8 @@ function App() {
       property1: "property1",
       property2: "property2",
     })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
+    .then((response) => console.log("Created Resource:", response))
+    .catch((error) => console.log("Created Resource:", error));
 
   // Updating a resource
   axios
@@ -32,18 +32,34 @@ function App() {
       title: "new title",
       body: "new body",
     })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
+    .then((response) => console.log("Updated Resource:", response))
+    .catch((error) => console.log("Updated Resource:", error));
 
-  // Patching a part of a resource
+  // Patching part of a resource
   axios
     .patch("https://jsonplaceholder.typicode.com/posts/1", {
       title: "an even newer title",
     })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
+    .then((response) => console.log("Patched Resource:", response))
+    .catch((error) => console.log("Patched Resource:", error));
 
   // Deleting a resource
+  axios
+    .delete("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => console.log("Deleted Resource:", response))
+    .catch((error) => console.log("Deleted Resource:", error));
+
+  // Filtering resources
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts/?userID=1")
+    .then((response) => console.log("Filtered Resource:", response))
+    .catch((error) => console.log("Filtered Resource:", error));
+
+  // Getting nested resources
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts/1/comments")
+    .then((response) => console.log("Nested Resource:", response))
+    .catch((error) => console.log("Nested Resource:", error));
 
   return (
     <div className="App">
